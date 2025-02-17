ee56ea41b8111d7dfd62f46d2204ae7ea78e5c69
Rugved <rdhorje67@gmail.com>
2025-02-17 17:48:17 +0530

    loading animation change to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
+"use client";
 import HomePage from "@/components/HomePage";
 import Services from "@/components/Services";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <div>
-      <HomePage />
-      <Services />
-    </div>
+    <>
+      {loading ? (
+        <video autoPlay muted className="w-full h-screen object-cover">
+          <source
+            src="/WhatsApp Video 2025-02-15 at 1.01.23 PM.mp4"
+            type="video/mp4"
+          />
+        </video>
+      ) : (
+        <div>
+          <HomePage />
+          <Services />
+        </div>
+      )}
+    </>
   );
 }
